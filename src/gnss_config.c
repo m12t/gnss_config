0b8c4705b85471837260cd5a2b7b1f122f4d65ec
Michael Willett <mikerwillett@gmail.com>
2022-05-22 01:12:34 -0500

    added ability to extract the new baud rate from the message and update the pico to use this baud rate after transmitting the message to then receive messages on that frequency

--- src/gnss_config.c
+++ src/gnss_config.c
@@ -9,7 +9,7 @@
 #include "hardware/irq.h"
 
 #define UART_ID uart1   // change as needed
-#define BAUD_RATE 9600  // default BAUD rate for the module, can be changed below with a PUBX msg.
+#define BAUD_RATE 115200  // default BAUD rate for the module, can be changed below with a PUBX msg.
 #define DATA_BITS 8
 #define STOP_BITS 1
 #define PARITY UART_PARITY_NONE
@@ -22,6 +22,7 @@ void uart_tx_setup(void);
 void uart_rx_setup(void);
 void compile_message(char *nmea_msg, char *raw_msg, char *checksum,
                      char *terminator);
+int extract_baud_rate(char *string);
 
 /* ublox m8 datasheet: (I'm using a M8030 chip)
 https://content.u-blox.com/sites/default/files/products/documents/u-blox8-M8_ReceiverDescrProtSpec_UBX-13003221.pdf
@@ -105,63 +106,73 @@ void uart_rx_setup(void) {
     // uint8_t msg[] = { 0xB5,0x62,0x0A,0x04,0x00,0x00,0x0E,0x34,00 };
 }
 
+
+int extract_baud_rate(char *string) {
+    // extract the new baud from the message
+    printf("extracting baud rate....\n");
+    char *token;
+    token = strtok(string, ",");
+    for (int i=0; i<5; i++) {
+        // skip to the 5th field of the message
+        token = strtok(NULL, ",");
+    }
+    return atoi(token);
+}
+
+
 int main(void) {
     stdio_init_all();  // important so that printf() works
     uart_init(UART_ID, BAUD_RATE);
 
     // here are some NMEA PUBX messages to be modified as needed.
     // checksum values (immediately following `*`) are generated automatically
-    char update_baud_rate[] = "$PUBX,41,1,3,3,115200,0*";  // cs=(1C) update baud rate to `115200`
-    char enable_zda[] = "$PUBX,40,ZDA,1,1,1,0*";           // cs=(45) enable ZDA
-    char disable_gsv[] = "$PUBX,40,GSV,0,0,0,0*";          // cs=(59) disable GSV
-    char disable_vtg[] = "$PUBX,40,VTG,0,0,0,0*";          // cs=(5E) disable VTG
-    char disable_rmc[] = "$PUBX,40,RMC,0,0,0,0*";          // cs=(47) disable RMC
-    char disable_gsa[] = "$PUBX,40,GSA,0,0,0,0*";          // cs=(4E) disable GSA
-    char disable_gll[] = "$PUBX,40,GLL,0,0,0,0*";          // cs=(5C) disable GLL messages
+    char update_baud_rate[] = "$PUBX,41,1,3,3,115200,0*";  // update baud rate
+    char enable_zda[] = "$PUBX,40,ZDA,1,1,1,0*";           // enable ZDA
+    char disable_gsv[] = "$PUBX,40,GSV,0,0,0,0*";          // disable GSV
+    char disable_vtg[] = "$PUBX,40,VTG,0,0,0,0*";          // disable VTG
+    char disable_rmc[] = "$PUBX,40,RMC,0,0,0,0*";          // disable RMC
+    char disable_gsa[] = "$PUBX,40,GSA,0,0,0,0*";          // disable GSA
+    char disable_gll[] = "$PUBX,40,GLL,0,0,0,0*";          // disable GLL messages
 
-    // modify these variables to control execution:
-    char raw_msg[] = "$PUBX,40,ZDA,1,1,1,0*";  // pick the desired message to write and just hardcode it here
-    int write_msg = 1;  // 1 will write the nmea_msg to UART, 0 will skip the write but execute everything else.
-    // --------------- end modifyable parameters ---------------
+    // --------------- modify these variables to control execution---------------:
+
+    char raw_msg[] = "$PUBX,40,GLL,0,0,0,0*";  // pick the desired message to write and just hardcode it here
+    int write_msg = 0;  // 1 will write the nmea_msg to UART, 0 will skip the write but execute everything else.
+
+    // ------------------------ end modifyable parameters ------------------------
 
     char checksum[2];  // placeholder for checksum
-    strcpy(checksum, "");
+    strcpy(checksum, "");  // initialize to empty string to avoid junk values
     int cs;
     cs = get_checksum(raw_msg);  // calc the hex checksum and write it to the `checksum` array
     sprintf(checksum, "%x", cs);  // convert the decimal checksum to hexadecimal
     // itoa(cs, checksum, 16);  // alternative to sprintf()
-    printf("%s", checksum);
+    // printf("%s", checksum);  // for debugging
     char msg_terminator[] = "\r\n";  // NMEA sentence terminator <cr><lr> == "\r\n"
-    size_t msg_len = strlen(raw_msg);
-    size_t term_len = strlen(msg_terminator);
-    size_t cs_len = strlen(checksum);
-    printf("msg_len: %lu", msg_len);
-    printf("term_len: %lu", term_len);
-    printf("cs_len: %lu", cs_len);
-    char nmea_msg[msg_len + term_len + cs_len];
+    char nmea_msg[strlen(raw_msg) + strlen(msg_terminator) + strlen(checksum)];
     strcpy(nmea_msg, "");  // initialize to empty string to avoid junk values
-
-    compile_message(nmea_msg, raw_msg, checksum, msg_terminator);
+    compile_message(nmea_msg, raw_msg, checksum, msg_terminator);  // assemble the components into the final msg
 
     uart_tx_setup();  // initialize UART on the pico
 
     if (write_msg) {
         // fire off the message multiple times. changing BAUD_RATE in particular definitely needs this treatment.
-        // char nmea_msg[] = "$PUBX,41,1,3,3,115200,0*1C\r\n";  // used to guarantee execution while the above is beign debugged.
+        printf("firing off message...\n");
         for (int k = 0; k < 5; k++) {
             // send these prior to interrupts
-            printf("\n<><><><><><><><><>\n");
             for (int i=0; i<strlen(nmea_msg); i++) {
                 uart_putc_raw(UART_ID, nmea_msg[i]);
             }
-            printf("<><><><><><><><><>\n");
         }
         if (strncmp(raw_msg, update_baud_rate, 23) == 0) {
+            int new_baud;
+            new_baud = extract_baud_rate(update_baud_rate);
             // update the pico's UART baud rate to the newly set value.
-            printf("updating baud rate");
-            int __unused actual = uart_set_baudrate(UART_ID, 115200);
+            printf("updating baud rate to %d", new_baud);
+            int __unused actual = uart_set_baudrate(UART_ID, new_baud);
         }
     }
+    sleep_ms(500);
     uart_rx_setup();
     while (1)
         tight_loop_contents();
